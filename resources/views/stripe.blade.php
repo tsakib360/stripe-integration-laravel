46dcafab80176be57dc5c0dfe8780f0b764e6ac1
tsakib360 <71313906+tsakib360@users.noreply.github.com>
2021-04-27 17:21:01 +0600

    DB integrated

--- resources/views/stripe.blade.php
+++ resources/views/stripe.blade.php
@@ -105,11 +105,12 @@
 
                         <input type="hidden" name="amount" value="{{$amount}}">
                         <input type="hidden" name="invoice" value="{{ uniqid()}}">
+                        
                         <div class='form-row row'>
 
                             <div class='col-xs-12 form-group required'>
 
-                                <label class='control-label'>Name on Card</label> 
+                                <label class='control-label'>Name on Card</label>
                                 <input class='form-control' size='4' type='text' value="Test">
 
                             </div>
@@ -122,7 +123,7 @@
 
                             <div class='col-xs-12 form-group card required'>
 
-                                <label class='control-label'>Card Number</label> 
+                                <label class='control-label'>Card Number</label>
                                 <input autocomplete='off' class='form-control card-number' size='20' type='text' value="4242 4242 4242 4242">
 
                             </div>
@@ -135,20 +136,20 @@
 
                             <div class='col-xs-12 col-md-4 form-group cvc required'>
 
-                                <label class='control-label'>CVC</label> 
+                                <label class='control-label'>CVC</label>
                                 <input autocomplete='off' class='form-control card-cvc' placeholder='ex. 311' size='4' type='text' value="123">
                             </div>
 
                             <div class='col-xs-12 col-md-4 form-group expiration required'>
 
-                                <label class='control-label'>Expiration Month</label> 
+                                <label class='control-label'>Expiration Month</label>
                                 <input class='form-control card-expiry-month' placeholder='MM' size='2' type='text' value="12">
 
                             </div>
 
                             <div class='col-xs-12 col-md-4 form-group expiration required'>
 
-                                <label class='control-label'>Expiration Year</label> 
+                                <label class='control-label'>Expiration Year</label>
                                 <input class='form-control card-expiry-year' placeholder='YYYY' size='4' type='text' value="2024">
 
                             </div>
